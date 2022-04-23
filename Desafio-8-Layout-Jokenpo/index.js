89378d75ee8d486fd1f675329bdc18857e23ff16
Tadrio Tabosa <tadrio_88@hotmail.com>
2022-04-23 17:50:38 -0300

    Finish
    Finish the challenge by addind the requested features as:
    the leading zero at the score.
    te start/pause text.

--- Desafio-8-Layout-Jokenpo/index.js
+++ Desafio-8-Layout-Jokenpo/index.js
@@ -1,21 +1,45 @@
 const $rockButton1 = document.querySelector(".player-1-rock-button");
 const $paperButton1 = document.querySelector(".player-1-paper-button");
-const $scissorsButton1 = document.querySelector(".player-1-scissors-button");
+const $scissorButton1 = document.querySelector(".player-1-scissor-button");
 
 const $rockButton2 = document.querySelector(".player-2-rock-button");
 const $paperButton2 = document.querySelector(".player-2-paper-button");
-const $scissorsButton2 = document.querySelector(".player-2-scissors-button");
+const $scissorButton2 = document.querySelector(".player-2-scissor-button");
 
 const $resetButton = document.querySelector(".reset-button");
+const $startButton = document.querySelector(".start-button")
 
 const $player1Field = document.querySelector(".player-1-field");
 const $player2Field = document.querySelector(".player-2-field");
 
 const $winnerTitle = document.querySelector(".winner-title");
 
+const $scorePlayer1 = document.querySelector(".score-player-1");
+const $scorePlayer2 = document.querySelector(".score-player-2");
+
 let movePlayer1 = "-";
 let movePlayer2 = "";
 let gameResult = null;
+let scorePlayer1 = 0;
+let scorePlayer2 = 0;
+let gameStart = false;
+
+function printScoreboard() {
+  if (scorePlayer1<10){
+  $scorePlayer1.innerHTML = '0'+scorePlayer1;}
+  else {$scorePlayer1.innerHTML = scorePlayer1}
+  if (scorePlayer2<10){
+    $scorePlayer2.innerHTML = '0'+scorePlayer2;}
+    else {$scorePlayer2.innerHTML = scorePlayer2}
+}
+
+function addPoint(winnerNumber) {
+  if (winnerNumber == 1) {
+    scorePlayer1++;
+  } else if (winnerNumber == 2) {
+    scorePlayer2++;
+  }
+}
 
 function verifyGame() {
   if (
@@ -33,60 +57,91 @@ function verifyGame() {
   } else if (movePlayer1 == movePlayer2) {
     gameResult = 0;
   }
+  addPoint(gameResult);
 }
 
 function printGameResult() {
   if (gameResult == 0) {
     $winnerTitle.innerHTML = "Empatou!";
-  } else if (gameResult == 1){
-    $winnerTitle.innerHTML = "Jogadora 1 ganhou!"
-  } else if (gameResult == 2){
-    $winnerTitle.innerHTML = "Jogadora 2 ganhou!"
+  } else if (gameResult == 1) {
+    $winnerTitle.innerHTML = "Jogadora 1 ganhou!";
+  } else if (gameResult == 2) {
+    $winnerTitle.innerHTML = "Jogadora 2 ganhou!";
   } else {
-    $winnerTitle.innerHTML = "Resultado"
+    $winnerTitle.innerHTML = "Resultado";
+  }
+  printScoreboard();
+}
+
+function resetMoveVariables() {
+  movePlayer1 = "";
+  movePlayer2 = "";
+}
+
+function resetBattlefield() {
+  $player1Field.innerHTML = "";
+  $player2Field.innerHTML = "";
+  $winnerTitle.innerHTML = "Resultado";
+}
+
+function move(playerNumber, moveName) {
+  if (gameStart){
+    if (playerNumber == 1) {
+      $player1Field.innerHTML =
+        '<img class="move-image" src="./assets/' + moveName + '.png" />';
+      movePlayer1 = moveName;
+    } else if (playerNumber == 2) {
+      $player2Field.innerHTML =
+        '<img class="move-image" src="./assets/' + moveName + '.png" />';
+      movePlayer2 = moveName;
+    }
+    verifyGame();
+    printGameResult();
+    if (gameResult != null) {
+      setTimeout(resetBattlefield, 1000);
+      resetMoveVariables();
+      gameResult = null;
+    }
   }
 }
 
 $rockButton1.addEventListener("click", function () {
-  $player1Field.innerHTML = '<img class="move-image" src="/assets/rock.png" />';
-  movePlayer1 = "rock";
-  verifyGame();
-  printGameResult();
+  move(1, "rock");
 });
 $paperButton1.addEventListener("click", function () {
-  $player1Field.innerHTML = '<img class="move-image" src="/assets/paper.png"/>';
-  movePlayer1 = "paper";
-  verifyGame();
-  printGameResult();
+  move(1, "paper");
 });
-$scissorsButton1.addEventListener("click", function () {
-  $player1Field.innerHTML =
-    '<img class="move-image" src="/assets/scissors.png"/>';
-  movePlayer1 = "scissor";
-  verifyGame();
-  printGameResult();
+$scissorButton1.addEventListener("click", function () {
+  move(1, "scissor");
 });
 
 $rockButton2.addEventListener("click", function () {
-  $player2Field.innerHTML = '<img class="move-image" src="/assets/rock.png" />';
-  movePlayer2 = "rock";
-  verifyGame();
-  printGameResult();
+  move(2, "rock");
 });
 $paperButton2.addEventListener("click", function () {
-  $player2Field.innerHTML = '<img class="move-image" src="/assets/paper.png"/>';
-  movePlayer2 = "paper";
-  verifyGame();
-  printGameResult();
+  move(2, "paper");
 });
-$scissorsButton2.addEventListener("click", function () {
-  $player2Field.innerHTML =
-    '<img class="move-image" src="/assets/scissors.png"/>';
-  movePlayer2 = "scissor";
-  verifyGame();
-  printGameResult();
+$scissorButton2.addEventListener("click", function () {
+  move(2, "scissor");
 });
 
-$resetButton.addEventListener("click", function(){
-  location.reload();
-})
+$resetButton.addEventListener("click", function () {
+  //location.reload();
+  resetBattlefield();
+  resetMoveVariables();
+  scorePlayer1 = 0;
+  scorePlayer2 = 0;
+  printScoreboard();
+});
+
+$startButton.addEventListener("click", function () {
+  gameStart = !gameStart;
+  $startButton.classList.toggle('start');
+  if (gameStart){
+    $startButton.innerHTML = 'Pausar'
+  }
+  else{
+    $startButton.innerHTML = 'Iniciar'
+  }
+});
+  console.log(movePlayer1,movePlayer2,gameResult,scorePlayer1,scorePlayer2,gameStart)
